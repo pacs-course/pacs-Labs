1a523e36a746d84addd8952a632c0ba1e3802b42
alberto <Alberto.Artoni@polimi.it>
2024-03-11 09:58:13 +0000

    Lab shortened: from 3h to 2h

--- Labs/2024/03-functional/02-newton/main.cpp
+++ Labs/2024/03-functional/02-newton/main.cpp
@@ -1,7 +1,29 @@
-#include "newton.hpp"
+#include "NewtonSolver.hpp"
 
-int
-main(int argc, char **argv)
+// function f 
+double fun(const double &x)
 {
+  return x*x*x + 5 * x + 3;
+};
+
+// derivative of f
+double dfun(const double &x)
+{
+  return 3 *x*x + 5;
+};
+
+int main(int argc, char **argv)
+{
+  // initialize solver
+  NewtonSolver solver(fun, dfun);
+
+  // run Newton solver with initial condition 0.0 
+  solver.solve(0.0);
+
+  // output the results
+  std::cout << "x =    " << solver.get_result() << std::endl;
+  std::cout << "r =    " << solver.get_residual() << std::endl;
+  std::cout << "iter = " << solver.get_iter() << std::endl;
+
   return 0;
 }
