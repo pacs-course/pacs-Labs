714e398d9183e7d078e30973f50bc57276fac765
alberto <Alberto.Artoni@polimi.it>
2024-03-21 11:19:37 +0000

    check

--- Labs/2024/05-stl-templates/02-derivatives-solution/main.cpp
+++ Labs/2024/05-stl-templates/02-derivatives-solution/main.cpp
@@ -16,7 +16,7 @@ auto timeit(const std::function<void()> &f, size_t N = 1) {
 
 
 int main(int argc, char **argv) {
-  const size_t N = 10'000'000; 
+  const size_t N = 1e8; 
   const double h = 1e-2;
 
   const auto f = [](double x) { return x*x*x*x*x; };
@@ -46,11 +46,20 @@ int main(int argc, char **argv) {
     const auto y = TemplateBoolRecursionNthDerivative<4, true>{f, h}(1.0);
   };
 
+  const auto test_template_v3 = [&]() {
+    const auto y (NthDerivative<4, std::function<double(double)>, 
+                  double, Differentiation::FORWARD >{f, h}(1.0));
+  };
+
   std::cout << "Test hard coded:               " << timeit(test_manual, N) << "[ms]\n";
   std::cout << "Test naive impl:               " << timeit(test_naive_v1, N) << "[ms]\n";
   std::cout << "Test naive impl (alternating): " << timeit(test_naive_v2, N) << "[ms]\n";
   std::cout << "Test templated (v1):           " << timeit(test_template_v1, N) << "[ms]\n";
   std::cout << "Test templated (v2):           " << timeit(test_template_v2, N) << "[ms]\n";
+  std::cout << "Test templated (v3):           " << timeit(test_template_v3, N) << "[ms]\n";
   std::cout << "Test templated (full):         " << timeit(test_template, N) << "[ms]\n";
+  std::cout << "Test hard coded:               " << timeit(test_manual, N) << "[ms]\n";
+  std::cout << "check accuracy: " << NthDerivative<4, std::function<double(double)>>{f, h}(1.0) - df4(1.0) << std::endl;
+  std::cout << "check accuracy: " << make_forward_derivative<4>(f, h)(1.0) - df4(1.0) << std::endl;
 }
 
