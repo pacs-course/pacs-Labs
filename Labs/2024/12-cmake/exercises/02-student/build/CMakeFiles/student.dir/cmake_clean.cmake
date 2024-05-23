45b9bd9b36dd6f96ce5394a19f59062e54b9f2a6
alberto <Alberto.Artoni@polimi.it>
2024-05-23 15:43:14 +0200

    Lab 12

---
+++ Labs/2024/12-cmake/exercises/02-student/build/CMakeFiles/student.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student.dir/src/Student.cpp.o"
+  "CMakeFiles/student.dir/src/Student.cpp.o.d"
+  "CMakeFiles/student.dir/test.cpp.o"
+  "CMakeFiles/student.dir/test.cpp.o.d"
+  "student"
+  "student.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
